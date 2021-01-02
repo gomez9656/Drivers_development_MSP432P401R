e548b1f2b4bb8face288c96242d90c0cae3d86f3
Juan Pablo Gomez Garcia <juanpg9656@gmail.com>
2021-01-02 10:21:04 -0500

    Firsts lines of driver development

---
+++ Drivers/Inc/msp432p401r.h
@@ -0,0 +1,18 @@
+/*
+ * msp432p401r.h
+ *
+ *  Created on: 24/09/2020
+ *      Author: PC
+ */
+
+#ifndef DRIVERS_INC_MSP432P401R_H_
+#define DRIVERS_INC_MSP432P401R_H_
+
+#define FLASH_BASEADDR              0x00000000U
+#define SRAM1_BASEADDR              0x20000000U
+#define ROM                         0x02000000U
+#define SRAM                        SRAM1_BASEADDR
+
+
+
+#endif /* DRIVERS_INC_MSP432P401R_H_ */
